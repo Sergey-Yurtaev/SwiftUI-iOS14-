cab935ffdb48ecee1693ef5a933a4a023b87888e
Sergey-Yurtaev <engineerk2@mail.ru>
2022-07-21 23:49:21 +0500

    Initial Commit

---
+++ SwiftUI(iOS14)/SwiftUI_iOS14_App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_iOS14_App.swift
+//  SwiftUI(iOS14)
+//
+//  Created by  Sergey Yurtaev on 21.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_iOS14_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
